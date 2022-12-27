1950c70d94f37ce0e8ae4c14ea2059fe7fadba29
Candide Kemmler <hi@candide.me>
2022-12-27 17:55:10 +0100

    scaffold plugin boilerplate

---
+++ src/main/SolidMainComponent.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function SekundMainComponent() {
+	return (
+		<div>
+			<h4>Hello, Solid!</h4>
+		</div>
+	);
+}
---
+++ src/main/SolidView.tsx
@@ -0,0 +1,34 @@
+import SolidPlugin from "@/main";
+import SolidMainComponent from "@/main/SolidMainComponent";
+import { ItemView, WorkspaceLeaf } from "obsidian";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export const SOLID_VIEW_TYPE = "solid-view";
+
+export default class SolidView extends ItemView {
+	plugin: SolidPlugin;
+	constructor(leaf: WorkspaceLeaf, plugin: SolidPlugin) {
+		super(leaf);
+		this.plugin = plugin;
+	}
+
+	getViewType() {
+		return SOLID_VIEW_TYPE;
+	}
+
+	getDisplayText() {
+		return "Solid View";
+	}
+
+	async onOpen() {
+		const container = this.containerEl.children[1];
+		container.empty();
+		ReactDOM.render(<SolidMainComponent />, this.containerEl.children[1]);
+		// this.plugin.updateOnlineStatus();
+	}
+
+	async onClose() {
+		// Nothing to clean up.
+	}
+}
